0125185248890118aeb09ebe386179bc8b27b5d7
Galaxieed <charleslpillos@gmail.com>
2024-05-22 11:30:14 +0800

    fixed animation

--- frontend/src/root.jsx
+++ frontend/src/root.jsx
@@ -5,44 +5,48 @@ import WaveBackground from "./components/shared-components/animated-background/w
 import NavBarY from "./components/shared-components/navbar-vertical/navbarY";
 import Header from "./components/shared-components/header/header";
 import SideProfile from "./components/shared-components/side-profile/side-profile";
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { useEffect, useState } from 'react';
 
 export default function Root() {
     const location = useLocation();
+    const [animationClass, setAnimationClass] = useState('upward-fade-enter');
+
+    useEffect(() => {
+        // Add the entering animation class
+        setAnimationClass('upward-fade-enter');
+
+        // Clean up by removing the class after animation ends
+        const timer = setTimeout(() => {
+            setAnimationClass('');
+        }, 500); // Match the duration of the CSS animation
+
+        return () => clearTimeout(timer);
+    }, [location]);
 
     return (
         <main>
             <WaveBackground />
-            <Container maxWidth={"xl"}>
+            <Container maxWidth="xl">
                 <Grid container spacing={2} mt={2}>
-                    <Grid item xs md={12}>
+                    <Grid item xs={12}>
                         <Header />
                     </Grid>
-                    <Grid item xs md={3}>
+                    <Grid item xs={12} md={3}>
                         <SideProfile />
                     </Grid>
-                    <Grid item xs md={8}>
-                        <TransitionGroup>
-                            <CSSTransition
-                                key={location.key}
-                                classNames="fade"
-                                timeout={300}>
-                                <Outlet />
-                            </CSSTransition>
-                        </TransitionGroup>
+                    <Grid item xs={12} md={8}>
+                        <div className={animationClass}>
+                            <Outlet />
+                        </div>
                     </Grid>
-                    <Grid item xs md={1}>
+                    <Grid item xs={12} md={1}>
                         <NavBarY />
                     </Grid>
-                    <Grid item xs md={12}>
-                        <Footer/>
+                    <Grid item xs={12}>
+                        <Footer />
                     </Grid>
                 </Grid>
-                
-                
-                    
-                
             </Container>
         </main>
-    )
+    );
 }
